5276637163758d107e8e6d39692d693facbc9e26
harshitraaaj <harshitraj31074@gmail.com>
2024-03-20 14:52:22 +0530

    modifying files

--- String.c
+++ String.c
@@ -1,52 +1,64 @@
-#include<stdio.h> 
-#include<string.h> 
-int nfind(char *str,char *pat)  { 
- int i,j,start=0; 
- int lasts=strlen(str)-1; 
- int lastp=strlen(pat)-1; 
- int endmatch=lastp; 
- for(i=0;endmatch<=lasts;endmatch++,start++)  { 
-  if(str[endmatch]==pat[lastp])  { 
-   j=0; 
-   for(i=start;j<lastp && str[i]==pat[j];i++) 
-    j++; 
-   if(j==lastp) 
-    return start; 
-  } 
- } 
- return -1; 
-} 
-void StrReplace(char *string,char *pat,char *Rep) { 
- int i,j; 
- char Res[50]; 
- int pos=0; 
- while(pos!=-1)  { 
-  pos=nfind(string,pat); 
-  Res[0]='\0'; 
-  if(pos==-1) 
-   printf("\n %s is not found in %s",pat,string); 
-  else 
-{ 
-   printf("\n %s is found at pos %d in  %s",  
-                                             pat, pos, string); 
-   for(i=0;i<pos;i++) 
-    Res[i]=string[i]; 
-   for(j=0;j<strlen(Rep);j++) 
-    Res[i++]=Rep[j]; 
-   for(j=pos+strlen(pat);string[j]!='\0';j++) 
-    Res[i++]=string[j]; 
-   Res[i]='\0'; 
-   printf("\nAfter Replace %s\n",Res); 
-   for(i=0;i<strlen(Res);i++) 
-    string[i]=Res[i]; 
-   string[i]='\0'; 
-  } 
- } 
-} 
-void main() 
-{ 
- char string[]={"AADAACAADAABAADAA"}; 
- char pat[]={"ADAA"}; 
- char Rep[]={"XXX"}; 
- StrReplace(string,pat,Rep); 
+#include <stdio.h>
+#include <conio.h>
+void read(char *s)
+{
+  gets(s);
+}
+
+void strcopy(char *s1, char *s2)
+{
+  int i;
+  for (i = 0; s2[i] != '\0'; i++)
+    s1[i] = s2[i];
+  s1[i] = '\0';
+}
+int matchnreplace(char *str, char *pat, char *rep)
+{
+  char ans[100];
+  int i = 0, m = 0, c = 0, j = 0, flag = 0, k;
+  while (str[c] != '\0')
+  {
+    if (str[m] == pat[i])
+    {
+      i++;
+      m++;
+      if (pat[i] == '\0')
+      {
+        flag = 1;
+        for (k = 0; rep[k] != '\0'; k++, j++)
+          ans[j] = rep[k];
+        i = 0;
+        c = m;
+      }
+    }
+    else
+    {
+      ans[j] = str[c];
+      j++;
+      c++;
+      m = c;
+      i = 0;
+    }
+  }
+  ans[j] = '\0';
+  strcopy(str, ans);
+  return flag;
+}
+
+int main()
+{
+  char str[100], pat[20], rep[20];
+  int flag = 0;
+  printf("Enter the string : ");
+  read(str);
+  printf("Enter the pattern : ");
+  read(pat);
+  printf("Enter the string to be replaced : ");
+  read(rep);
+  flag = matchnreplace(str, pat, rep);
+  if (flag == 1)
+    printf("The string is found %s", str);
+  else
+    printf("The pattern is not found");
+  return 0;
 }
